59c36983a0acb05c40b699ca8cdc903311d80373
Sajidali123 <q.sajid36"gmail.com>
2020-05-13 16:26:17 +0500

    files uploaded

---
+++ src/pages/util/customTheme.js
@@ -0,0 +1,75 @@
+export default {
+    form: {
+        textAlign: 'center'
+      },
+      image: {
+        width: '120px',
+        height: 'auto'
+       },
+      textField: {
+      margin: '10px auto 10px auto'
+      },
+      button: {
+      marginTop: 20,
+      postition: 'relative'
+      },
+    customError: {
+      color: 'red',
+      fontSize: '0.8rem',
+      marginTop: 10
+    },
+    progress:{
+      postition: 'absolute'
+    },
+    submitButton: {
+      postition: 'relative',
+      float: 'right',
+      marginTop: 10
+    },
+    progressSpinner:{
+      position: 'absolute'
+    },
+    closeButton: {
+      position: 'absolute',
+      left: '91%',
+      top: '6%'
+
+    },
+    invisibleSeperator: {
+      border: 'none',
+      margin: 4
+    },
+    visibleSeperator: {
+      width: '100%',
+      borderBottom: '1px solid rgba(0,0,0,0.1)',
+      marginBottom: 20
+    },
+    profileImage: {
+      maxWidth: 200,
+      height: 200,
+      borderRadius: '50%',
+      objectFit: 'cover'
+    },
+    dialogContent: {
+      padding: 20
+    },
+    expandButton: {
+      postition: 'absolute',
+      left: '90%'
+    },
+    spinnerDiv: {
+      textAlign: 'center',
+      marginTop: 50,
+      marginBottom: 50
+     },
+     commentImage: {
+       maxWidth: '100%',
+       height: 100,
+       objectFit: 'cover',
+       borderRadius: '50%'
+     },
+     commentData: {
+       marginLeft: 20
+     }
+
+}
